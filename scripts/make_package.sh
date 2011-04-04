7f90d73807fd0085fc3935e903155032c53defff
Dav Glass <davglass@gmail.com>
2011-04-04 14:02:58 -0500

    Moved yui3-yui3 file to be a 'built' file as well.

--- scripts/make_package.sh
+++ scripts/make_package.sh
@@ -23,6 +23,20 @@ cp LICENSE $dir
 wait
 cp -R lib $dir
 wait
+#Copy yui3-yui3.js into main file
+
+echo "" >> $dir/lib/node-yui3.js
+echo "" >> $dir/lib/node-yui3.js
+echo "/**" >> $dir/lib/node-yui3.js
+echo "  Imported from ./lib/yui3-yui3.js" >> $dir/lib/node-yui3.js
+echo "**/" >> $dir/lib/node-yui3.js
+echo "" >> $dir/lib/node-yui3.js
+echo "" >> $dir/lib/node-yui3.js
+cat $dir/lib/yui3-yui3.js >> $dir/lib/node-yui3.js
+wait
+rm $dir/lib/yui3-yui3.js
+
+#Copy RLS into main file
 echo "" >> $dir/lib/node-yui3.js
 echo "" >> $dir/lib/node-yui3.js
 echo "/**" >> $dir/lib/node-yui3.js
